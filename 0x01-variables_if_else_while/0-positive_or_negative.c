4bc0d50578b30a56ea590f356b08ce07f7e7de24
Peace Jackson <peacyjackson@gmail.com>
2023-05-27 07:53:58 -0700

    positive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * n - variable in which number is assigned to
+ * return 0
+ */
+
+int main(void)
+{
+	int n;
+	
+	n = 0;
+
+	if (n < 0)
+	{
+		printf("%d is negative/n", n);
+	}
+else if (n > 0)
+{
+	printf("%d is positive/n", n);
+}
+return (0);
+}
