ed0aa3ae5f8ac19f69d0f889ffc5509fba67b089
Peace Jackson <peacyjackson@gmail.com>
2023-05-28 10:41:29 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - a program that prints exactly
+ * programming is like building a multilingual
+ * puzzle
+ * return 0
+ */
+
+char greetings[58] = {"programming is like building a multilingual puzzle"};
+{
+	{
+	puts("%s", greetings);
+	}
+	return (0);
+}
+
